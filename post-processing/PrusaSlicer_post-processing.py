ca6987e56d36f684ac31f077efb515582aefc837
danveitch76 <45300973+danveitch76@users.noreply.github.com>
2020-11-26 15:01:13 +0000

    Disregard header
    Disregards header and adds layer height on actual first print move.
    
    Added Old Slic3r Post Processor which did a simlar thing.

--- post-processing/PrusaSlicer_post-processing.py
+++ post-processing/PrusaSlicer_post-processing.py
@@ -5,6 +5,7 @@ import sys
 import os
 
 FILE_PATH = sys.argv[1]
+# FILE_PATH = "Robot_Maker_Faire_150pc_0.2mm_PLA_Ender-3_100_20h42m3.gcode"
 
 # first_layer_height
 global FIRST_LAYER_H
@@ -14,7 +15,7 @@ FIRST_LAYER_H_CLEAN = ''
 
 FIRST_MOVE_GCODE = 'G1 X'
 
-REPLACEMENT = '; Z moved to first X move\n'
+REPLACEMENT = ''
 COMBINE_ALERT = '; Combine Z and XY move\n'
 
 global FIRST_MOVE_PATCHED
@@ -22,34 +23,36 @@ FIRST_MOVE_PATCHED = False
 global COMBINE_ALERT_PATCHED
 COMBINE_ALERT_PATCHED = False
 
+global HEADER
+HEADER = '; END Header'
+global AFTER_HEADER
+AFTER_HEADER = False
+
 DEBUG = False
 
 # Open input file
 file_input = open(FILE_PATH, "r")
-# Open output file 
+# Open output file
 file_output = open(FILE_PATH + ".new", "w")
 
+print('Searching for layer height...')
 
 for search_line in file_input:
-  if DEBUG:
-    print('Search for parameters...')
   if 'first_layer_height = ' in search_line:
     get_first_layer_height = search_line.split("=")
     get_first_layer_height = get_first_layer_height[1].rstrip("\n").lstrip(" ")
-    FIRST_LAYER_H = 'G1 Z' + get_first_layer_height 
-    FIRST_LAYER_H_CLEAN = 'Z' + get_first_layer_height + '\n'
+    FIRST_LAYER_H = 'G1 Z' + get_first_layer_height
+    FIRST_LAYER_H_CLEAN = 'Z' + get_first_layer_height
   elif COMBINE_ALERT in search_line:
     COMBINE_ALERT_PATCHED = True
-    
 
-if DEBUG:
-  print('----- Variables ---------')
-  print('File: ' + FILE_PATH)
-  print('Filst layer H:' + FIRST_LAYER_H)
-  print('Layer marker: "' + FIRST_LAYER_H + '"')
-  print('-------------------------')
-  print('')
- 
+
+print('Layer height found')
+print('File: ' + FILE_PATH)
+print('First layer height:' + get_first_layer_height)
+print('Layer height marker: "' + FIRST_LAYER_H + '"')
+print('Adding layer height to first XY move...')
+
 
 # Move cursor to begining
 file_input.seek(0)
@@ -58,8 +61,10 @@ file_input.seek(0)
 for line in file_input:
   if DEBUG:
     print('Line: ' + line)
-  if FIRST_LAYER_H in line and FIRST_MOVE_PATCHED is False: 
-    if DEBUG: 
+  if HEADER in line and AFTER_HEADER is False:
+    AFTER_HEADER = True
+  if FIRST_LAYER_H in line and FIRST_MOVE_PATCHED is False and AFTER_HEADER is True:
+    if DEBUG:
       print('############################')
       print('Match found in line: ' + line)
       print('############################')
@@ -67,20 +72,19 @@ for line in file_input:
       print('Replaced line' + REPLACEMENT)
       print('-------------------------------------')
     file_output.write(REPLACEMENT)
-  elif FIRST_MOVE_GCODE in line and FIRST_MOVE_PATCHED is False and FIRST_LAYER_H_CLEAN not in line and COMBINE_ALERT_PATCHED is False: 
+  elif FIRST_MOVE_GCODE in line and FIRST_MOVE_PATCHED is False and FIRST_LAYER_H_CLEAN not in line and COMBINE_ALERT_PATCHED is False and AFTER_HEADER is True:
     if DEBUG:
       print('####################')
       print('Patching first move')
       print('Old first move: ' + line)
-      print('New first move: ' + line.rstrip("\n") + ' ' + FIRST_LAYER_H_CLEAN)
-    file_output.write(COMBINE_ALERT)
-    file_output.write(line.rstrip("\n") + ' ' + FIRST_LAYER_H_CLEAN)
-    file_output.write('; ----------------------\n')
+      print('New first move: ' + line[0:line.rfind(";")].rstrip() + ' ' + FIRST_LAYER_H_CLEAN + ' ' + line[line.rfind(";"):len(line)].rstrip("/n"))
+    file_output.write(line[0:line.rfind(";")].rstrip() + ' ' + FIRST_LAYER_H_CLEAN + ' ' + line[line.rfind(";"):len(line)].rstrip("/n"))
     FIRST_MOVE_PATCHED = True
   else:
     file_output.write(line)
-    
-  
+
+print('Completed update')
+print('Saving file...')
 # Close files
 file_input.close()
 file_output.close()
@@ -88,4 +92,4 @@ file_output.close()
 # Replace files
 os.remove(FILE_PATH)
 os.rename(FILE_PATH + ".new", FILE_PATH)
-
+print('File saved.')
